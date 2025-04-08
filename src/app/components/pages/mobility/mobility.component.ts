481fd809475c84a40ba57e0076b6a807210cc48b
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-04-08 13:13:13 +0530

    mobility

--- src/app/components/pages/mobility/mobility.component.ts
+++ src/app/components/pages/mobility/mobility.component.ts
@@ -19,7 +19,13 @@ export class MobilityComponent {
     { title: 'Enhanced Customer Experience', description: 'Provide your customers with a seamless and responsive service experience, enhancing satisfaction and loyalty.' },
     { title: 'Competitive Advantage', description: 'Stay ahead of the competition with cutting-edge technology that keeps your business agile and innovative.' }
   ];
-
+  gridItems = [
+    { title: 'Seamless Integration', description: 'Our solutions easily integrate with existing enterprise systems and platforms, ensuring a smooth transition and minimal disruption to your operations.', image: 'assets/images/seamless_integration.jpeg' },
+    { title: 'Enhanced Security', description: 'Protecting your data is our top priority. We offer advanced security features, including encryption, multi-factor authentication, and remote wipe capabilities, to safeguard sensitive information against cyber threats.', image: 'assets/images/enhance_security.jpeg' },
+    { title: 'Customisable Applications', description: 'We provide a range of customisable mobile applications designed to streamline workflows and increase operational efficiency. These applications are intuitive and user friendly, requiring minimal training and support.', image: 'assets/images/custom_application.jpeg' },
+    { title: 'Real-time Analytics', description: 'Gain insights into your business operations with real-time data analytics. Our solutions offer detailed reports and analytics to help you make informed decisions and stay ahead of the competition.', image: 'assets/images/realtimeanalysis.jpeg' },
+    { title: 'Scalable Solutions', description: 'As your business grows, our mobility solutions can scale to meet your evolving needs. Whether you\'re expanding your workforce or entering new markets, we have the technology and expertise to support your growth.', image: 'assets/images/scalable.jpeg' }
+  ];
   /**
    * Toggles the accordion item's expanded state
    * @param event The click event
